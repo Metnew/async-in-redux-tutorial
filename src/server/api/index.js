c8548b4bea81a54f13824efad01245b31b8fdd4e
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-01-06 01:48:45 +0200

    fix(server): remove unused api routes
    fix(server): remove unused api routes

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,11 +1,8 @@
 import {Router} from 'express'
-import auth from './auth'
-import links from './links.json'
 const router = Router()
 
-router.use('/auth', auth)
-router.get('/links', (req, res) => {
-	res.send(links)
+router.get('/data', (req, res) => {
+	res.send({ok: 'You received this data from server!'})
 })
 
 export default router
