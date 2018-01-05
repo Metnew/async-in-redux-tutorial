099b9a2b20f6c058c50b6ed0a1a9a7648695841e
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-01-06 01:58:13 +0200

    fix(selectors): remove unused selectors
    fix(selectors): remove unused selectors

--- src/common/selectors/index.js
+++ src/common/selectors/index.js
@@ -1,15 +1,9 @@
 // @flow
 import {createSelector} from 'reselect'
 import {routingFnCreator} from 'routing'
-import type {State as AuthState} from 'reducers/auth'
-import type {State as LayoutState} from 'reducers/layout'
-import type {State as EntitiesLinksState} from 'reducers/links'
-import type {GlobalState} from 'reducers'
 
-export const getAuthState = (state: GlobalState): AuthState => state.auth
-export const getLayoutState = (state: GlobalState): LayoutState => state.layout
-export const getEntitiesLinksState = (state: GlobalState): EntitiesLinksState =>
-	state.entities.links
+export const getLayoutState = state => state.layout
+export const getStorageState = state => state.storage
 
 export const getWindowInnerWidth = (window: Object): number => {
 	const defaultWindowInnerWidth = 1025
@@ -32,3 +26,6 @@ export const getRoutes = routingFnCreator()
 export const getMetaRoutes = routingFnCreator('meta')
 export const getSidebarRoutes = routingFnCreator('sidebar')
 export const getRouterRoutes = routingFnCreator('routing')
+
+export const isLoaded = state => state.fetchStatus === 'loaded'
+export const isLoading = state => state.fetchStatus === 'loading'
