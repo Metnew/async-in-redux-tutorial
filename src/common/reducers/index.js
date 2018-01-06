9c8f51bfbf373f35951a009e04de15bedbc0d3cf
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-01-06 02:08:28 +0200

    fix(reducers): remove unused and tests
    fix(reducers): remove unused and tests

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -3,25 +3,13 @@ import {combineReducers} from 'redux'
 import {routerReducer} from 'react-router-redux'
 import {reducer as reduxFormReducer} from 'redux-form'
 
-import type {State as AuthState} from 'reducers/auth'
-import type {State as LayoutState} from 'reducers/layout'
-import type {State as EntitiesLinksState} from 'reducers/links'
-
 import {layout} from './layout'
-import {links} from './links'
-import {auth} from './auth'
+import {storage} from './storage'
 
 // Root reducer
 export default combineReducers({
 	layout,
-	auth,
-	entities: combineReducers({
-		links
-	}),
+	storage,
 	routing: routerReducer,
 	form: reduxFormReducer
 })
-
-export type GlobalState = {layout: LayoutState} & {auth: AuthState} & {
-		entities: {links: EntitiesLinksState}
-	}
--- src/common/reducers/layout/index.js
+++ src/common/reducers/layout/index.js
@@ -1,49 +1,33 @@
 // @flow
-import {
-	UI_TOGGLE_SIDEBAR,
-	UI_WINDOW_RESIZE
-} from 'actions/layout'
+import {UI_TOGGLE_SIDEBAR, UI_WINDOW_RESIZE} from 'actions/layout'
 import {LOCATION_CHANGE} from 'actions/common'
-import type {LOCATION_CHANGE_TYPE} from 'actions/common'
-import type {
-	UI_TOGGLE_SIDEBAR_TYPE,
-	UI_WINDOW_RESIZE_TYPE
-} from 'actions/layout'
-
-export type State = {
-	sidebarOpened: boolean,
-	innerWidth?: number
-}
-
-type Action =
-	| UI_TOGGLE_SIDEBAR_TYPE
-	| UI_WINDOW_RESIZE_TYPE
-	| LOCATION_CHANGE_TYPE
+import typeToReducer from 'type-to-reducer'
 
 export const initialState: State = {
 	sidebarOpened: false
 }
 
-export function layout (state: State = initialState, action: Action): State {
-	switch (action.type) {
-	case UI_WINDOW_RESIZE: {
-		const {innerWidth} = action.payload
-		return {
-			...state,
-			innerWidth
+export const layout = typeToReducer(
+	{
+		[UI_WINDOW_RESIZE] (state, action) {
+			const {innerWidth} = action.payload
+			return {
+				...state,
+				innerWidth
+			}
+		},
+		[UI_TOGGLE_SIDEBAR] (state) {
+			return {
+				...state,
+				sidebarOpened: !state.sidebarOpened
+			}
+		},
+		[LOCATION_CHANGE] (state) {
+			return {
+				...state,
+				sidebarOpened: false
+			}
 		}
-	}
-	case UI_TOGGLE_SIDEBAR:
-		return {
-			...state,
-			sidebarOpened: !state.sidebarOpened
-		}
-	case LOCATION_CHANGE:
-		return {
-			...state,
-			sidebarOpened: false
-		}
-	default:
-		return state
-	}
-}
+	},
+	initialState
+)
