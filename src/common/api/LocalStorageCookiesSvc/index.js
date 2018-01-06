5fccc820863bc875072c80174fd1e20889bf5579
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-01-06 02:44:31 +0200

    fix(api): remove LocalStorageCookiesSvc
    fix(api): remove LocalStorageCookiesSvc

--- src/common/api/LocalStorageCookiesSvc/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import Cookies from 'js-cookie'
-export const JWT_TOKEN = 'JWT_TOKEN'
-
-export function getLocalToken (): string | null {
-	return Cookies.get(JWT_TOKEN)
-}
-
-export const isLoggedIn = () => !!getLocalToken()
